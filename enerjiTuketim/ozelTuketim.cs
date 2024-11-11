9de910670b39fee44938f044fcd6830beda3e612
Murat Abbaslı <allahverdi.abbasli.1995@gmail.com>
2024-11-11 13:50:00 +0300

    Update ozelTuketim.cs
    240 kWh üstü ve altı metodlarına 24 saat için uyarı koşulu eklendi
    diğer dosyalardaki metodlarda bulunan hesaplama formülleri eklendi

--- enerjiTuketim/ozelTuketim.cs
+++ enerjiTuketim/ozelTuketim.cs
@@ -1,32 +1,83 @@
+using System;
+
 namespace enerjiTuketim
 {
-    public class ozelTuketim()
+    public class ozelTuketim
     {
         public static void ozel240kWhalti() // 11. Seçim
-         {
+        {
+            Console.Clear();
             Console.WriteLine("Cihazın güç(W) değerini giriniz : ");
             double watt = double.Parse(Console.ReadLine());
             Console.WriteLine("Çalışma süresini(Saat) giriniz  : ");
-            double sure = double.Parse(Console.ReadLine());           
-            double kiloWatt = watt/1000D;
-            double normalTuketimBedeli = 2.07d;
+            double sure = double.Parse(Console.ReadLine());
+            if (sure < 0 || sure > 24)
+            {
+               Console.WriteLine("Verdiğiniz bilgiler dahilinde doğru sonuçlar alamayabilirsiniz.");
+            }
 
-            double toplamNormalTuketimBedeli = (kiloWatt*sure)*normalTuketimBedeli;
+            double kiloWatt = watt / 1000D; // kW hesaplama
+            double normalTuketimBedeli = 2.07d; // TL birim başına
+            double gunlukTuketim = kiloWatt * sure; // Günlük tüketim kW cinsinden
 
-            Console.WriteLine($"Elektrik tüketim değeri = {kiloWatt}kWh\nToplam normal tüketim bedeli: {toplamNormalTuketimBedeli}");
-         }
-         public static void ozel240kWhuzeri() //11. Seçim
-         {
+            double[] zamanCarpanlari = { 1, 7, 30, 365 };
+            string[] zamanDilimi = { "1 gün", "1 hafta", "1 ay", "1 yıl" };
+
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            for (int i = 0; i < zamanCarpanlari.Length; i++)
+            {
+                double toplamTuketim = gunlukTuketim * zamanCarpanlari[i];
+                double toplamBedel = toplamTuketim * normalTuketimBedeli;
+
+                Console.WriteLine($"{sure} saatlik kullanım için yaklaşık {zamanDilimi[i]}de {toplamTuketim:F2} kW elektrik, {toplamBedel:F2} TL fatura.");
+            }
+
+            double toplamNormalTuketimBedeli = gunlukTuketim * normalTuketimBedeli;
+            Console.WriteLine($"Elektrik tüketim değeri = {kiloWatt}kWh\nGünlük toplam normal tüketim bedeli: {toplamNormalTuketimBedeli:F2} TL");
+            Console.WriteLine("\nNot:Sonuçlar değişkenlik gösterebilir.\nFiyatlar 2024 yılı için geçerlidir.\nDevam etmek için 'Enter' tuşuna basınız.");
+            Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Clear();
+        }
+
+        public static void ozel240kWhuzeri() // 11. Seçim
+        {
+            Console.Clear();
             Console.WriteLine("Cihazın güç(W) değerini giriniz : ");
             double watt = double.Parse(Console.ReadLine());
             Console.WriteLine("Çalışma süresini(Saat) giriniz  : ");
-            double sure = double.Parse(Console.ReadLine());           
-            double kiloWatt = watt/1000D;
+            double sure = double.Parse(Console.ReadLine());
+            if (sure < 0 || sure > 24)
+            {
+               Console.WriteLine("Verdiğiniz bilgiler dahilinde doğru sonuçlar alamayabilirsiniz.");
+            }
+
+            double kiloWatt = watt / 1000D;
             double yuksekTuketimBedeli = 3.11d;
+            double gunlukTuketim = kiloWatt * sure;
 
-            double toplamYuksekTuketimBedeli = (kiloWatt*sure)*yuksekTuketimBedeli;
+            double[] zamanCarpanlari = { 1, 7, 30, 365 };
+            string[] zamanDilimi = { "1 gün", "1 hafta", "1 ay", "1 yıl" };
 
-            Console.WriteLine($"Elektrik tüketim değeri = {kiloWatt}kWh\nToplam yüksek tüketim bedeli: {toplamYuksekTuketimBedeli}");          
-         }
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            for (int i = 0; i < zamanCarpanlari.Length; i++)
+            {
+                double toplamTuketim = gunlukTuketim * zamanCarpanlari[i];
+                double toplamBedel = toplamTuketim * yuksekTuketimBedeli;
+
+                Console.WriteLine($"{sure} saatlik kullanım için yaklaşık {zamanDilimi[i]}de {toplamTuketim:F2} kW elektrik, {toplamBedel:F2} TL fatura.");
+            }
+
+            double toplamYuksekTuketimBedeli = gunlukTuketim * yuksekTuketimBedeli;
+            Console.WriteLine($"Elektrik tüketim değeri = {kiloWatt}kWh\nToplam yüksek tüketim bedeli: {toplamYuksekTuketimBedeli:F2} TL");
+            Console.WriteLine("\nNot:Sonuçlar değişkenlik gösterebilir.\nFiyatlar 2024 yılı için geçerlidir.\nDevam etmek için 'Enter' tuşuna basınız.");
+            Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Clear();
+        }
     }
 }
