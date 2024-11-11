9c2d1108395a413d386c4840aad7006fd78f00ad
Murat Abbaslı <allahverdi.abbasli.1995@gmail.com>
2024-11-11 13:51:28 +0300

    Update bulasikMakinesi.cs
    Hesaplamaya iki değer arasında rastgele sonuçlar veren bölümler eklendi.

--- enerjiTuketim/bulasikMakinesi.cs
+++ enerjiTuketim/bulasikMakinesi.cs
@@ -2,10 +2,9 @@ namespace enerjiTuketim
 {
     public class BulasikMakinesi 
     {
-        public static void Calistir()
+        public static void Calistir() // 1. Seçim
         {
-            Console.Clear();
-            const double tuketimBedeli = 2.07;
+            const double tuketimBedeli = 2.07D;
             Console.Write("Bulaşık Makinesi çalışma süresini giriniz (saat): ");
             int calismaSuresi = Convert.ToInt32(Console.ReadLine());
 
@@ -13,6 +12,7 @@ namespace enerjiTuketim
             double[] zamanCarpanlari = { 1, 7, 30, 365 };
             string[] zamanDilimi = { "1 gün", "1 hafta", "1 ay", "1 yıl" };
 
+            Console.Clear();
             Console.ForegroundColor = ConsoleColor.Red;
 
             for (int i = 0; i < zamanCarpanlari.Length; i++)
@@ -20,16 +20,21 @@ namespace enerjiTuketim
                 double toplamTuketim = gunlukTuketim * zamanCarpanlari[i];
                 double toplamBedel = toplamTuketim * tuketimBedeli;
                 
-                Console.WriteLine($"{calismaSuresi} saatlik kullanım için {zamanDilimi[i]}de {toplamTuketim:F2} kW elektrik, {toplamBedel:F2} TL fatura.");
+                Console.WriteLine($"{calismaSuresi} saatlik kullanım için yaklaşık {zamanDilimi[i]}de {toplamTuketim:F2} kW elektrik, {toplamBedel:F2} TL fatura.");
             }
-
-            Console.ResetColor();
+            Console.WriteLine("\nNot:Sonuçlar değişkenlik gösterebilir.\nFiyatlar 2024 yılı için geçerlidir.\nDevam etmek için 'Enter' tuşuna basınız.");
+            Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Clear();
+            
         }
 
         private static double HesaplaTuketim(double saat)
         {
-            const int minGuc = 1200;
-            return minGuc * saat / 1000; // Minimum güç üzerinden tüketimi döndür
+            Random minMaxGucTuketim = new Random();
+            int gucTuketim = minMaxGucTuketim.Next(1199,1500);
+            // const int minGuc = 1200;
+            return gucTuketim * saat / 1000; // 1200-1500 arasında değer döndürür.
         }
     }
 }
